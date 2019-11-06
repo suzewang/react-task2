6675c1b9b5372868f0cc0790ee64dbbd346742bd
suzewang <2072521628@qq.com>
2019-11-06 09:29:24 +0800

    11-6

--- src/container/Question.js
+++ src/container/Question.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-export default class All extends Component {
+export default class Question extends Component {
     constructor(){
         super();
         this.state={
--- src/container/job.js
+++ src/container/job.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-export default class All extends Component {
+export default class Job extends Component {
     constructor(){
         super();
         this.state={
